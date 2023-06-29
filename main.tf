c5f949ced47a35cbb9fbaacd0ecdcc750a11b842
Rakesh Chauhan <101490768+goodluck2717@users.noreply.github.com>
2023-06-29 09:54:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,24 @@
 provider "aws" {
     region = "eu-north-1"  
 }
+resource "aws_instance" "web" {
+  ami                     = "ami-04e4606740c9c9381"
+  instance_type           = "t3.micro"
+  #disable_api_termination = false
+  #disable_api_stop        = false
+  user_data               = <<-EOF
+          #!/bin/bash
+          yum update -y
+          yum install httpd -y
+          systemctl start httpd
+          systemctl enable httpd
+          yum install git -y
+          git clone https://github.com/goodluck2717/goodluck.git
+          mv /goodluck/* /var/www/html/
+          EOF
 
-resource "aws_instance" "foo" {
-  ami           = "ami-04e4606740c9c9381" # eu-north-1
-  instance_type = "t3.micro"
+
   tags = {
-      Name = "TF-Instance"
+    Name = "HelloWorld"
   }
 }
